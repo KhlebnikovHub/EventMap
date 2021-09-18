a2b77593bdd3ae9b1909e918e1f8ba21c436a5b4
Daniil <daniilnardin13@gmail.com>
2021-09-18 15:35:30 +0300

    release dragndrop

--- client/src/components/OneOfAllUsers/OneOfAllUsers.jsx
+++ client/src/components/OneOfAllUsers/OneOfAllUsers.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function OneOfAllUsers({ id, email, firstname, lastname, avatar }) {
+  if(!avatar?.includes('http')) {
+    avatar = `${process.env.REACT_APP_API_URL}${avatar}`
+  }
   return (
     <div>
       <br/>
-      <img src={avatar} style={{width: '300px'}}/>
+      <img src={`${avatar}`} style={{width: '300px'}}/>
       <p>{email}</p>
       <p>{firstname}</p>
       <p>{lastname}</p>
